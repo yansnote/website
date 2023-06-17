6457ee37981e497dfe655eb18c34dc745a9cd043
Yan Naing <tannaing@gmail.com>
2023-06-17 22:51:36 +0630

    Hobby part has been done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,10 +15,16 @@
   </section>
 
   <section class="hobbies">
-    <h3>Some of My Hobbies</h3>
+    <h3 class="section-header">Some of My Hobbies</h3>
 
     <div class="hobby-body">
-      <chip-comp title="Testing" />
+      <ChipComp
+        v-for="(hobby, i) in hobbies"
+        :key="i"
+        :title="hobby.title"
+        :icon="hobby.icon"
+        :color="hobby.color"
+      />
     </div>
   </section>
 </template>
@@ -26,14 +32,80 @@
 <script>
 import ChipComp from '@/components/ChipComp.vue'
 
+import CodeIcon from '@/assets/xml.svg'
+import PhotographyIcon from '@/assets/camera-outline.svg'
+import CoffeeIcon from '@/assets/coffee-outline.svg'
+import WritingIcon from '@/assets/feather.svg'
+import MovieIcon from '@/assets/television-classic.svg'
+import GameIcon from '@/assets/gamepad-variant-outline.svg'
+import CraftIcon from '@/assets/hammer-screwdriver.svg'
+import TravelIcon from '@/assets/plane-car.svg'
+import ReadIcon from '@/assets/book-open-page-variant-outline.svg'
+
 export default {
-  components: { ChipComp }
+  components: { ChipComp },
+  data: () => {
+    return {
+      hobbies: [
+        {
+          icon: CodeIcon,
+          title: 'Coding',
+          color: 'var(--teal-color)'
+        },
+        {
+          icon: PhotographyIcon,
+          title: 'Photography',
+          color: 'var(--yellow-color)'
+        },
+        {
+          icon: GameIcon,
+          title: 'Playing Games',
+          color: 'var(--orange-color)'
+        },
+        {
+          icon: MovieIcon,
+          title: 'Watching Movies',
+          color: 'var(--yellow-color)'
+        },
+        {
+          icon: WritingIcon,
+          title: 'Writing',
+          color: 'var(--blue-color)'
+        },
+        {
+          icon: ReadIcon,
+          title: 'Reading',
+          color: 'var(--base-color)'
+        },
+        {
+          icon: CraftIcon,
+          title: 'Crafting',
+          color: 'var(--teal-color)'
+        },
+        {
+          icon: CoffeeIcon,
+          title: 'Brewing Coffee',
+          color: 'var(--orange-color)'
+        },
+        {
+          icon: TravelIcon,
+          title: 'Travelling',
+          color: 'var(--yellow-color)'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-  .jumbotron {
-    margin-top: 80px;
+.section-header {
+  font-size: 1.82rem;
+  text-align: center;
+  margin-bottom: 40px;
+}
+.jumbotron {
+    margin-top: 100px;
     display: grid;
     grid-template-columns: minmax(320px, 3fr) minmax(320px, 2fr);
 
@@ -62,7 +134,17 @@ export default {
   }
 
   .hobbies {
-    margin-top: 80px;
+    margin-top: 100px;
+
+    .hobby-body {
+      width: 82%;
+      margin: 0 auto;
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      gap: 15px;
+      justify-content: center;
+    }
   }
 </style>
 ChipComp
