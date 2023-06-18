f099ffa1bdb349a31f5ababd50fb6c78c5af3836
Yan Naing <tannaing@gmail.com>
2023-06-18 18:41:04 +0630

    Finalizing first version

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -4,11 +4,21 @@
   </main>
 </template>
 
+
+
 <style lang="scss" scoped>
 .portfolio {
+  height: calc(100vh - 180px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (max-width: 720px) {
+    height: calc(100vh - 150px);
+  }
+
   h1 {
     text-align: center;
-    margin: 200px auto;
   }
 }
 </style>
