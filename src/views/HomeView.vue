e9b4de21b66abb12ca4e00af259d436a4174d5e9
Yan Naing <tannaing@gmail.com>
2023-06-18 13:30:03 +0630

    Timeline has been done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -27,10 +27,29 @@
       />
     </div>
   </section>
+
+  <section class="timeline-section">
+    <h3 class="section-header">My Timeline and Life Events</h3>
+
+    <div class="timeline-wrapper">
+      <div class="timeline-body">
+        <TimelineComp
+          v-for="(value, i) in timeline"
+          :key="i"
+          :title="value.title"
+          :date="value.date"
+          :icon="value.icon"
+          :color="value.color"
+          class="timeline-item"
+        />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 import ChipComp from '@/components/ChipComp.vue'
+import TimelineComp from '@/components/TimelineComp.vue'
 
 import CodeIcon from '@/assets/xml.svg'
 import PhotographyIcon from '@/assets/camera-outline.svg'
@@ -42,8 +61,16 @@ import CraftIcon from '@/assets/hammer-screwdriver.svg'
 import TravelIcon from '@/assets/plane-car.svg'
 import ReadIcon from '@/assets/book-open-page-variant-outline.svg'
 
+import LifeIcon from '@/assets/flag-checkered.svg'
+import EduIcon from '@/assets/school-outline.svg'
+import CareerIcon from '@/assets/account-tie-outline.svg'
+import MedalIcon from '@/assets/seal.svg'
+
 export default {
-  components: { ChipComp },
+  components: {
+    ChipComp,
+    TimelineComp
+  },
   data: () => {
     return {
       hobbies: [
@@ -92,6 +119,86 @@ export default {
           title: 'Travelling',
           color: 'var(--yellow-color)'
         }
+      ],
+      timeline: [
+        {
+          date: "2022 October 08",
+          icon: LifeIcon,
+          title: "Became a father",
+          color: "var(--orange-color)"
+        },
+        {
+          date: "2021 December 15",
+          icon: LifeIcon,
+          title: "Founded a family with Thida Aye",
+          color: "var(--orange-color)"
+        },
+        {
+          date: "2021 July 28",
+          icon: CareerIcon,
+          title: "Started working as Low-Code Automation Developer at Hana Microfinance",
+          color: "var(--teal-color)"
+        },
+        {
+          date: "2020 January 18",
+          icon: EduIcon,
+          title: "Got DMA (Diploma of Management Administration) from Yangon University of Economics",
+          color: "var(--yellow-color)"
+        },
+        {
+          date: "2018 November 12",
+          icon: CareerIcon,
+          title: "Started working as Business Analyst at Hana Microfinance",
+          color: "var(--teal-color)"
+        },
+        {
+          date: "2017 December 23",
+          icon: EduIcon,
+          title: "Got DTSM (Diploman in Tourism Management and Studies) from National Management Degree Collage",
+          color: "var(--yellow-color)"
+        },
+        {
+          date: "2017 July 01",
+          icon: CareerIcon,
+          title: "Changed career as Tour Operator at Croton Travles and Tour",
+          color: "var(--teal-color)"
+        },
+        {
+          date: "2014 October, 01",
+          icon: CareerIcon,
+          title: "Founded HEXCORES Web Development Team",
+          color: "var(--teal-color)"
+        },
+        {
+          date: "2014 March, 16",
+          icon: MedalIcon,
+          title: "Won the prize at Code for Change Myanmar Hackathon (Firstever hackathon held in Myanmar)",
+          color: "var(--blue-color)"
+        },
+        {
+          date: "2011 September, 01",
+          icon: CareerIcon,
+          title: "Started carrier life as a web developer at Myanmar Links",
+          color: "var(--teal-color)"
+        },
+        {
+          date: "2011 February, 01",
+          icon: EduIcon,
+          title: "Got B.A degree from Yadanabon University",
+          color: "var(--yellow-color)"
+        },
+        {
+          date: "2006",
+          icon: EduIcon,
+          title: "Finished basic education student life",
+          color: "var(--yellow-color)"
+        },
+        {
+          date: "1991 February, 12",
+          icon: LifeIcon,
+          title: "Hello World!",
+          color: "var(--orange-color)"
+        }
       ]
     }
   }
@@ -105,46 +212,62 @@ export default {
   margin-bottom: 40px;
 }
 .jumbotron {
-    margin-top: 100px;
-    display: grid;
-    grid-template-columns: minmax(320px, 3fr) minmax(320px, 2fr);
+  margin-top: 120px;
+  display: grid;
+  grid-template-columns: minmax(320px, 3fr) minmax(320px, 2fr);
 
-    .biography {
-      .bio-pre {
-        padding: 0;
-        margin: 0;
-      }
-      .bio-name {
-        font-size: 3rem;
-        margin: 0;
-      }
+  .biography {
+    .bio-pre {
+      padding: 0;
+      margin: 0;
     }
-
-    .face {
-      text-align: center;
-      img {
-        max-width: 75%;
-        margin: 0 auto;
-        border-radius: 30px;
-        border: 2px solid var(--bold-color);
-        box-shadow: 3px 3px 0 var(--bold-color);
-        margin: 0 auto;
-      }
+    .bio-name {
+      font-size: 3rem;
+      margin: 0;
     }
   }
 
-  .hobbies {
-    margin-top: 100px;
-
-    .hobby-body {
-      width: 82%;
+  .face {
+    text-align: center;
+    img {
+      max-width: 75%;
       margin: 0 auto;
-      display: flex;
-      flex-direction: row;
-      flex-wrap: wrap;
-      gap: 15px;
-      justify-content: center;
+      border-radius: 30px;
+      border: 2px solid var(--bold-color);
+      box-shadow: 3px 3px 0 var(--bold-color);
+      margin: 0 auto;
     }
   }
+}
+
+.hobbies {
+  margin-top: 150px;
+
+  .hobby-body {
+    width: 82%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 15px;
+    justify-content: center;
+  }
+}
+
+.timeline-section {
+  margin-top: 150px;
+
+  .timeline-wrapper {
+    display: flex;
+    justify-content: center;
+    .timeline-body {
+      display: inline-block;
+
+      .timeline-item {
+        margin-bottom: 20px;
+      }
+    }
+  }
+}
 </style>
 ChipComp
