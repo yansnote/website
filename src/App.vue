805768de68f03cc10e1fc0d261e303f2ef37818f
Yan Naing <tannaing@gmail.com>
2025-08-02 13:20:51 +0700

    Update vue 3.2 to 3.5

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,11 @@
-<template>
-  <main class="main">
-    <header-comp />
+<script setup lang="ts"></script>
 
-    <router-view/>
-  </main>
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
 </template>
 
-<script>
-import HeaderComp from '@/components/HeaderComp.vue'
-
-export default {
-  components: {
-    HeaderComp
-  }
-}
-</script>
-
-<style>
-
-@import url('https://fonts.googleapis.com/css2?family=Courier+Prime:wght@400;700&display=swap');
-
-:root {
-  --base-color: #fcf3e6;
-  --bold-color: #33342f;
-  --teal-color: #52C1C5;
-  --yellow-color: #FFC348;
-  --orange-color: #F26E55;
-  --blue-color: #4780BA;
-}
-
-body {
-  color: var(--bold-color);
-  font-size: 18px;
-  font-family: 'Courier Prime', 'Courier New', Courier, monospace;
-
-  @media screen and (max-width: 720px) {
-    margin: 0;
-  }
-}
-
-.bg-teal { background: var(--teal-color); }
-.bg-bold { background: var(--bold-color); }
-.bg-yellow { background: var(--yellow-color); }
-.bg-orange { background: var(--orange-color); }
-.bg-blue { background: var(--blue-color); }
-
-.main {
-  max-width: 980px;
-  border: 2px solid var(--bold-color);
-  border-radius: 2.5rem;
-  background: var(--base-color);
-  margin: 30px auto;
-  padding: 30px 50px;
-  box-sizing: border-box;
-
-  @media screen and (max-width: 920px) {
-    margin: auto;
-    padding: 30px;
-  }
-
-  @media screen and (max-width: 720px) {
-    padding: 20px;
-    margin: 0;
-    border: none;
-    border-radius: 0;
-  }
-}
-
-</style>
+<style scoped></style>
