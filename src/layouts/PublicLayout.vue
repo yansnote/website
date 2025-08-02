1eba9c696c7708dc83ca9d7fb26c90e265b59863
Yan Naing <tannaing@gmail.com>
2025-08-02 15:08:36 +0700

    Add app header

--- src/layouts/PublicLayout.vue
+++ src/layouts/PublicLayout.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="public-layout">
-    <header class="bg-blue-600 text-white p-4">
-      <nav class="flex gap-4">
-        <router-link to="/" class="hover:text-blue-200">Home</router-link>
-      </nav>
-    </header>
+  <div class="w-full md:py-3 md:px-5">
+    <div
+      class="w-full max-w-4xl mx-auto md:border-2 border-(--bold-color) bg-(--base-color) rounded-4xl"
+    >
+      <header class="flex justify-between items-center p-8">
+        <router-link to="/" class="flex items-center size-12">
+          <img src="/Logo.svg" alt="YansNote Logo" />
+        </router-link>
+        <nav class="flex gap-4">
+          <router-link to="/" class="hover:text-blue-200">Home</router-link>
+        </nav>
+      </header>
 
-    <main class="p-4">
-      <router-view />
-    </main>
+      <main class="p-4">
+        <router-view />
+      </main>
 
-    <footer class="bg-gray-100 p-4 text-center text-gray-600 mt-8">
-      <p>© 2025 YansNote - Public Area</p>
-    </footer>
+      <footer class="bg-gray-100 p-4 text-center text-gray-600 mt-8">
+        <p>© 2025 YansNote - Public Area</p>
+      </footer>
+    </div>
   </div>
 </template>
