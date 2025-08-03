c0995e11c3d5a139d9d5b070f5b3923f23e50226
Yan Naing <tannaing@gmail.com>
2025-08-03 21:40:59 +0700

    Implement firebase firestore for social links

---
+++ src/components/home/SocialItem.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { computed, defineProps } from 'vue'
+
+import GithubIcon from '@/assets/images/icons/github.svg'
+import LinkedInIcon from '@/assets/images/icons/linkedin.svg'
+import XIcon from '@/assets/images/icons/twitter.svg'
+import FacebookIcon from '@/assets/images/icons/facebook.svg'
+
+const props = defineProps<{
+  name: string
+  link: string
+}>()
+
+const icon = computed(() => {
+  switch (props.name) {
+    case 'Github':
+      return GithubIcon
+    case 'LinkedIn':
+      return LinkedInIcon
+    case 'X':
+      return XIcon
+    case 'Facebook':
+      return FacebookIcon
+    default:
+      return ''
+  }
+})
+
+const color = computed(() => {
+  switch (props.name) {
+    case 'Github':
+      return 'yn-blue'
+    case 'LinkedIn':
+      return 'yn-yellow'
+    case 'X':
+      return 'yn-teal'
+    case 'Facebook':
+      return 'yn-orange'
+    default:
+      return ''
+  }
+})
+</script>
+<template>
+  <a
+    :href="link"
+    class="inline-flex items-center mr-4 p-1 rounded-full border-2 border-bold"
+    :class="'bg-' + color"
+  >
+    <img :src="icon" :alt="name" class="size-7" />
+  </a>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,14 @@
 import { Timestamp } from 'firebase/firestore'
 import FirestoreService from '@/firebase/firestore'
 
-import HeroSection from '@/components/home/HeroSection.vue'
 import HobbySection from '@/components/home/HobbySection.vue'
 
+import SocialItem from '@/components/home/SocialItem.vue'
 import TimelineItem from '@/components/home/TimelineItem.vue'
 
-import { onMounted, ref } from 'vue'
+import profileImage from '@/assets/images/YanNaing.jpg'
+
+import { computed, onMounted, ref } from 'vue'
 
 interface TimelineItemType {
   id: string
@@ -16,13 +18,24 @@ interface TimelineItemType {
   date: Timestamp
 }
 
+interface InfoType {
+  id: string
+  type: string
+  name: string
+  link: string | null
+}
+
 const timeline = ref<TimelineItemType[]>([])
+const infos = ref<InfoType[]>([])
+
+const socials = computed(() => infos.value.filter((info) => info.type === 'social'))
+const hobbies = computed(() => infos.value.filter((info) => info.type === 'hobby'))
 
 const fetchData = async () => {
   try {
-    const [timelineData, hobbies] = await Promise.all([
+    const [timelineData, info] = await Promise.all([
       FirestoreService.getCollection('timeline', [FirestoreService.orderBy('date', 'desc')]),
-      FirestoreService.getCollection('info', [FirestoreService.where('type', '==', 'hobby')]),
+      FirestoreService.getCollection('info'),
     ])
 
     timeline.value = (timelineData as TimelineItemType[]).map((item) => ({
@@ -32,7 +45,16 @@ const fetchData = async () => {
       date: item.date,
     }))
 
-    console.log('Hobbies:', hobbies)
+    infos.value = (info as InfoType[]).map((item) => ({
+      id: item.id,
+      type: item.type,
+      name: item.name,
+      link: item.link,
+    }))
+
+    console.log('Info:', infos.value)
+    console.log('Socials:', socials.value)
+    console.log('Hobbies:', hobbies.value)
   } catch (error) {
     console.error('Error fetching data:', error)
   }
@@ -45,7 +67,23 @@ onMounted(() => {
 
 <template>
   <section class="w-full px-8 mt-8">
-    <HeroSection />
+    <div class="flex justify-between items-start gap-5">
+      <div>
+        <h3 class="text-xl font-bold">Hello! I am</h3>
+        <h3 class="text-5xl font-bold">Yan Naing</h3>
+        <p class="mt-2">
+          I am currently a power platform consultant at United Nations ESCAP. I am also a freelance
+          web developer, a husband and a father.
+        </p>
+        <div class="mt-3">
+          <SocialItem v-for="(item, i) in socials" :key="i" :name="item.name" :link="item.link!" />
+        </div>
+      </div>
+
+      <figure class="w-1/2 max-w-3xs rounded-2xl overflow-hidden border-2 border-bold">
+        <img :src="profileImage" alt="Yan Naing" class="w-full" />
+      </figure>
+    </div>
   </section>
 
   <section class="w-full px-8 mt-18">
