b9acba8605429a65472a0b9ffd8bd5e2ee522938
Yan Naing <tannaing@gmail.com>
2025-08-03 14:29:13 +0700

    Implement firebase firestore data in timeline items

---
+++ src/components/home/TimelineItem.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import { computed, defineProps } from 'vue'
+import EventIcon from '@/assets/images/icons/event.svg'
+import AchievementIcon from '@/assets/images/icons/achievement.svg'
+import EducationIcon from '@/assets/images/icons/education.svg'
+import JobIcon from '@/assets/images/icons/job.svg'
+import { Timestamp } from 'firebase/firestore'
+
+const props = defineProps<{
+  date: Date | Timestamp
+  description: string
+  type: string
+}>()
+
+const color = computed(() => {
+  switch (props.type) {
+    case 'life-event':
+      return 'bg-yn-orange'
+    case 'education':
+      return 'bg-yn-yellow'
+    case 'achievement':
+      return 'bg-yn-blue'
+    case 'job':
+      return 'bg-yn-teal'
+    default:
+      return 'bg-gray-200'
+  }
+})
+
+const icon = computed(() => {
+  switch (props.type) {
+    case 'life-event':
+      return EventIcon
+    case 'education':
+      return EducationIcon
+    case 'achievement':
+      return AchievementIcon
+    case 'job':
+      return JobIcon
+    default:
+      return ''
+  }
+})
+
+const eventDate = computed(() => {
+  const dateObj = props.date instanceof Timestamp ? props.date.toDate() : new Date(props.date)
+
+  return dateObj.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  })
+})
+</script>
+
+<style scoped>
+.rail {
+  position: absolute;
+  width: 2px;
+  background-color: #ccc;
+  height: calc(100% - 26px);
+  top: 35px;
+  left: 15px;
+}
+</style>
+
+<template>
+  <div class="flex items-start gap-4 relative">
+    <div class="rail"></div>
+    <figure class="p-1 border-2 border-bold rounded-full" :class="color">
+      <img :src="icon" alt="Timeline Icon" class="size-5 mx-auto" />
+    </figure>
+    <div class="border-2 border-bold rounded-2xl py-2 px-4 bg-white">
+      <h4 class="text-sm text-gray-500 font-bold">{{ eventDate }}</h4>
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,46 @@
 <script setup lang="ts">
+import { Timestamp } from 'firebase/firestore'
+import FirestoreService from '@/firebase/firestore'
+
 import HeroSection from '@/components/home/HeroSection.vue'
 import HobbySection from '@/components/home/HobbySection.vue'
-import TimelineSection from '@/components/home/TimelineSection.vue'
+
+import TimelineItem from '@/components/home/TimelineItem.vue'
+
+import { onMounted, ref } from 'vue'
+
+interface TimelineItemType {
+  id: string
+  type: string
+  description: string
+  date: Timestamp
+}
+
+const timeline = ref<TimelineItemType[]>([])
+
+const fetchData = async () => {
+  try {
+    const [timelineData, hobbies] = await Promise.all([
+      FirestoreService.getCollection('timeline', [FirestoreService.orderBy('date', 'desc')]),
+      FirestoreService.getCollection('info', [FirestoreService.where('type', '==', 'hobby')]),
+    ])
+
+    timeline.value = (timelineData as TimelineItemType[]).map((item) => ({
+      id: item.id,
+      type: item.type,
+      description: item.description,
+      date: item.date,
+    }))
+
+    console.log('Hobbies:', hobbies)
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+}
+
+onMounted(() => {
+  fetchData()
+})
 </script>
 
 <template>
@@ -14,6 +53,16 @@ import TimelineSection from '@/components/home/TimelineSection.vue'
   </section>
 
   <section class="w-full px-8 my-18">
-    <TimelineSection />
+    <h3 class="text-2xl font-bold text-center">My Timeline and Life Events</h3>
+
+    <div class="mt-4 space-y-3">
+      <TimelineItem
+        v-for="item in timeline"
+        :key="item.id"
+        :date="item.date"
+        :description="item.description"
+        :type="item.type"
+      />
+    </div>
   </section>
 </template>
